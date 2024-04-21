9f669a21f73e5ad977bf7a7a493161949b769aa4
Klaas Decock <klaas.decock@cm.be>
2024-04-21 12:23:34 +0200

    chapter 14 io - advanced apis

---
+++ 14-file-io/src/main/java/advanced_apis/ReadAttributes.java
@@ -0,0 +1,23 @@
+package advanced_apis;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class ReadAttributes {
+
+    public static void main(String[] args) throws IOException {
+
+        Path path = Paths.get("test-directory");
+
+        BasicFileAttributes attributes = Files.readAttributes(path, BasicFileAttributes.class);
+        System.out.println("Is directory: " + attributes.isDirectory());
+        System.out.println("Creation time: " + attributes.creationTime());
+        System.out.println("Size: " + attributes.size() + " bytes");
+
+    }
+
+}
